6b5af393b001b398677d7b348e38a2907043bb96
jdalarconl <alarconlopezjuandavid@gmail.com>
2021-11-13 23:47:00 -0500

    prueba

--- resources/views/admin/edit_form.blade.php
+++ resources/views/admin/edit_form.blade.php
@@ -74,7 +74,7 @@
                 </div>
             </div>
         </div>
-        <p class="fw-bold my-3">Negocio</p>
+        <p class="fw-bold my-3">Negociossssss</p>
         <div class="card p-3 shadow-sm border-0 ">
             <div class="row">
                 <div class="col-12 col-md-6">
